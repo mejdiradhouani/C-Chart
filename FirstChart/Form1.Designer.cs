445a53a2ba45ad518cbb135e24a69509b7773696
MejdiRadhouai <MejdiRadhouai@MEJDI>
2017-05-11 22:41:01 +0100

    first commit

---
+++ FirstChart/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace FirstChart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.FirstChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.FirstChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FirstChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.FirstChart.ChartAreas.Add(chartArea1);
+            this.FirstChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.FirstChart.Legends.Add(legend1);
+            this.FirstChart.Location = new System.Drawing.Point(0, 0);
+            this.FirstChart.Name = "FirstChart";
+            this.FirstChart.Size = new System.Drawing.Size(578, 261);
+            this.FirstChart.TabIndex = 0;
+            this.FirstChart.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(462, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 21);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Afiicher courbe";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(462, 228);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 21);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Masquer la courbe";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.FirstChart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.FirstChart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart FirstChart;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ FirstChart/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace FirstChart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {    // ordonnée
+            
+           FirstChart.ChartAreas[0].AxisY.Maximum = 30.00;
+            FirstChart.ChartAreas[0].AxisY.Minimum = 0.00;
+            FirstChart.ChartAreas[0].AxisX.Maximum = 25;
+            FirstChart.ChartAreas[0].AxisX.Minimum = 0.00;
+            FirstChart.ChartAreas[0].AxisY.Title = "kV";
+
+            FirstChart.ChartAreas[0].AxisX.Title = "mA";
+
+            var serie = new Series("U");
+            serie.Color = Color.Blue;
+            serie.ChartType = SeriesChartType.Line;
+            serie.Points.DataBindXY(new[] { 4.4, 8.8 , 13.2, 15.84,18.48,20.24,22},
+                                            new[] { 0.34, 0.52, 1.06, 1.58,10.53,17.18,28.38 });
+            var serie1 = new Series("V");
+            serie1.Color = Color.Red;
+            serie1.ChartType = SeriesChartType.Line;
+            serie1.Points.DataBindXY(new[] { 4.4, 8.8, 13.2, 15.84, 18.48,20.24,22 },
+                                            new[] { 0.52,0.94, 1.45,2.11,2.90,4.24,6.03 });
+            var serie2 = new Series("W");
+            serie2.Color = Color.Green;
+            serie2.ChartType = SeriesChartType.Line;
+            serie2.Points.DataBindXY(new[] { 4.4, 8.8, 13.2, 15.84, 18.48, 20.24, 22 },
+                                            new[] { 0.45,0.57,1.67,3.12,4.93,6.59,8.18 });
+            serie.IsVisibleInLegend = true;
+            FirstChart.Series.Add(serie);
+            FirstChart.Series.Add(serie1);
+            FirstChart.Series.Add(serie2);
+            FirstChart.Legends.Add(new Legend("Automatiser des mésures"));
+
+            button2.Hide();
+            FirstChart.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.FirstChart.Show();
+            this.button1.Hide();
+            button2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FirstChart.Hide();
+            button1.Show();
+            button2.Hide();
+        }
+    }
+}
